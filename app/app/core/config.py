3be73e19e9ac8c18a19cec227abf96800d75bdc2
sandnima <sandnima33@gmail.com>
2022-02-01 02:10:52 +0330

    Routes registered in main file, bugs fixed, and requirements.txt updated
    - all routes included in main rout
    - Project name added in settings
    - Access token expire value bug fixed to return seconds' as integer
    - requirements.txt updated

--- app/app/core/config.py
+++ app/app/core/config.py
@@ -7,11 +7,13 @@ import app
 
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = "Auth Service"
+
     API_V1_STR: str = "/api/v1"
 
     SECRET_KEY: str = secrets.token_urlsafe(32)
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = datetime.timedelta(days=8)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = datetime.timedelta(days=8).seconds
 
     BASE_DIR = os.path.dirname(os.path.abspath(app.__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(BASE_DIR, 'database.db')
--- app/app/main.py
+++ app/app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
+app = FastAPI(
+    title=settings.PROJECT_NAME
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(api_router, prefix=settings.API_V1_STR)
